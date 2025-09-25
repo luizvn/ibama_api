c799c6ffead5478047d4d6a81a48ba1d36ead28b
luizvn <luizvinicius128@gmail.com>
2025-09-25 10:03:43 -0300

    build: add requiremments.in

--- app/core/config.py
+++ app/core/config.py
@@ -1,15 +1,17 @@
 import os
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import ClassVar
 
 class Settings(BaseSettings):
 
+    model_config = SettingsConfigDict(
+        env_file=".env", 
+        env_file_encoding="utf-8", 
+        extra="ignore"
+    )
+
     PROJECT_NAME: ClassVar[str] = "API de Autos de Infração - IBAMA"
 
     DATABASE_URL: str
-
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
 
 settings = Settings()
